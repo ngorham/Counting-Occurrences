fbd257358c06661587dbe8bdf75196486b7bc8bc
ngorham <ngorham2@gmail.com>
2018-02-19 12:40:17 -0800

    Count-Occurrences Update 1 2/19/2018
    Add countOccurrences.js: Javascript version of counting occurrences

---
+++ countOccurrences.js
@@ -0,0 +1,23 @@
+/**
+ * Counting Occurrences
+ * countOccurrences.js
+ * Purpose:
+ * 
+ * @author Neil Gorham
+ * @version 1.0.0 02/19/2018
+ */
+
+ //Count Occurrences Constructor
+ function CountOccurrences(){
+
+ }
+
+ //Merge Sort utility
+ function mergeSort(){}
+
+ //Counting Sort utility
+ function countingSort(){}
+
+ //Counts number of occurrences
+ function numOfOccurrences(){}
+ 
