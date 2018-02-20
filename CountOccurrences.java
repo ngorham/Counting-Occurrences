49184c7302a57afbeb5c1a2f404470426491a6c4
ngorham <ngorham2@gmail.com>
2018-02-19 16:58:28 -0800

    Counting-Occurrences Update 6 2/19/2018
    Added countOccurrences.java: java version

---
+++ CountOccurrences.java
@@ -0,0 +1,26 @@
+/**
+ * Counting Occurrences using Merge Sort
+ * CountOccurrences.java
+ * Purpose: Counts the number of occurrences of an integer in a given array
+ * 
+ * @author Neil Gorham
+ * @version 1.0.0 02/19/2018
+ */
+
+public class CountOccurrences(){
+	//Private variables
+
+	//Public Methods
+	//Main Constructor
+	public CountOccurrences(){}
+
+	//Main process
+	public static void main(String[] args){
+		CountOccurrences co = new CountOccurrences();
+		co.run();
+	}
+	//Private Methods
+	private void run(){
+		System.out.println("Count Occurrences using Merge Sort");
+	}
+}
